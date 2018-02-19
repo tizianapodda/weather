c3c3e5b7debcb50315e177f32acf393eb1175cb1
Tiziana <Tiziana@MacBook-Pro-di-Tiziana.local>
2018-02-19 21:44:40 +0100

    Add img

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 var is_debug = false;
 
-//Dichiarazione di funzione
 function showSection(section){
     $("section").hide();
     $("#"+section).show();
@@ -15,13 +14,16 @@ function getPosition(){
     {
       navigator.geolocation.getCurrentPosition(function (position) {
         getWeather(position.coords.latitude,position.coords.longitude);
-      });
+      });//altrimenti con il metodo getcurrentposition individuo la posizione del computer dell'utente che sta eseguendo l'applicazione
+      //mi darà quindi due coordinate di long lat
     }
 }
-
 function getWeather(lat, lng) {
       var url = "http://api.openweathermap.org/data/2.5/forecast/daily?cnt=7&units=metric&lat="+lat+"&lon="+lng+"&appid=581881e2788f16b15fe091b3bb64ce37";
-      //javascript object notation. Utilizziamo la funzione $.getJSON perchè passandogli un URL, lui fa una richiesta a questo URL e ritorna i dati presenti a questo URL in formato JSON un formato universale per l'interscambio di dati tra i vari linguaggi di programmazione
+      /*javascript object notation. Utilizziamo la funzione $.getJSON perchè passandogli un URL,
+     lui fa una richiesta a questo URL e ritorna i dati presenti a questo URL in formato JSON un
+     formato universale per l'interscambio di dati tra i vari linguaggi di programmazione*/
+
       $.getJSON(url)
       //Se la chiamata alle API va a buon fine
       .done(function( data ) {
@@ -45,8 +47,17 @@ function renderWeather(data){
     for (var i in data.list)
     {
         var container = $("#today-weather .row");
-        var date = new Date(data.list[i].dt*1000); //l'API ritorna la data in unix cioè in secondi dal 1 gennaio 1970, creando un oggetto della classe "Date"
+        /*istanzio un oggetto Date e lo assegno alla variabile date. L'oggetto Date accetta
+        come parametro il n totale di millisecondi trascorsi dal 1 gennaio 1970 ad oggi
+        per ritornare la data di oggi. L'Api però fornisce la data di oggi in secondi dal 1 gennaio 1970
+        quindi moltiplico per 1000 data.list[i].dt ovvero il percorso che raggiungo attraverso l'api e che
+        mi fornisce dt (la data di oggi in secondi)
+        */
+        var date = new Date(data.list[i].dt*1000);
         var day = ["Sunday", "Monday", "Tuensday", "Wednesday", "Thursday", "Friday", "Saturday"][date.getDay()];
+        //il metodo .getDay dell'oggetto Date ritorna il giorno di oggi in formato numerico da 0 a 6 dove 0 è domenica e 6 è sabato.
+        // alla variabile day associo il giorno che mi ritorna date.getDay
+        //api ritorna un array che in posizione 0 contiene sempre le previsioni di oggi
 
         if (i != 0){
           container = container.clone().appendTo("#day-list");
